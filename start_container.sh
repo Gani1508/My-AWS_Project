6b13a42bbb01e05f40e48ddf004aab00f659ef0c
Gani1508 <142162558+Gani1508@users.noreply.github.com>
2025-01-28 00:17:05 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-set -e
 
-# Pull the Docker image from Docker Hub
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 703671905249.dkr.ecr.us-east-1.amazonaws.com
-docker pull 703671905249.dkr.ecr.us-east-1.amazonaws.com/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-echo
